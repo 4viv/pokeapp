d9301c4eeda643f4147cc70bfebb103201045094
unknown <avivrazo@gmail.com>
2021-05-04 15:27:28 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { PerfilComponent } from './components/views/perfil/perfil.component';
+import { DetalleComponent } from './components/views/detalle/detalle.component';
+import { CatalogoComponent } from './components/views/catalogo/catalogo.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'catalogo', pathMatch: 'full'  },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'detalle/:id', component: DetalleComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: '**', redirectTo: 'catalogo', pathMatch: 'full'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CatalogoComponent } from './components/views/catalogo/catalogo.component';
+import { DetalleComponent } from './components/views/detalle/detalle.component';
+import { PerfilComponent } from './components/views/perfil/perfil.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoComponent,
+    DetalleComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
